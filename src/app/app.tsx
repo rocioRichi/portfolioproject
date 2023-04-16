a8a38dd8a3a8b48ac3c4e4f2f16e8bc37ba4898a
rocioRichi <paragit@gmail.com>
2023-04-16 14:51:41 +0200

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>Soy el header</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
